8d2dc8b2017b890aaf3c96a807e974f8555b6e49
Kristoffer <skogskogskog@gmail.com>
2019-12-07 11:00:24 +0100

    Day1

---
+++ Common/Readers/FileReader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Common.Readers
+{
+    public static class FileReader
+    {
+        public static List<T> GetList<T>(string path)
+        {
+            var result = new List<T>();
+            using (var reader = new StreamReader(path))
+            {
+
+                var line = reader.ReadLine();
+                while (line != null)
+                {
+                    result.Add(line.ChangeType<T>());
+                    line = reader.ReadLine();
+                }
+            }
+
+            return result;
+        }
+
+        static T ChangeType<T>(this object obj)
+        {
+            return (T)Convert.ChangeType(obj, typeof(T));
+        }
+    }
+}
---
+++ Tests/Advent01/ModulesTests.cs
@@ -0,0 +1,28 @@
+﻿using Advent01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests.Advent01
+{
+    public class ModulesTests
+    {
+
+        [Theory]
+        [InlineData(14, 2)]
+        public void GetAllRequiredFuel_ReturnsFuelBasedItterativelyOnFuel(int mass, int resultingFuel)
+        {
+            // Arrange
+            var module = new Module(mass);
+
+            // Act
+            var totalRequiredItterativeFuel = module.GetAllRequiredFuel();
+
+            // Assert
+            Assert.Equal(resultingFuel, totalRequiredItterativeFuel);
+        }
+    }
+}
